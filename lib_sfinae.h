787901709e44d3fcca18c3967bc3d04032a08201
Sandres92 <eastoutcity@mail.ru>
2025-08-30 12:57:36 +0000

    Add template to all neccsary types

--- lib_sfinae.h
+++ lib_sfinae.h
@@ -1,1 +1,171 @@
 #pragma once
+
+#include <utility>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cstdint>
+
+namespace otus
+{
+    template <bool conditional, typename T>
+    struct EnableIf;
+
+    template <typename T>
+    struct EnableIf<true, T>
+    {
+        using type = T;
+    };
+
+    template <typename T, typename U>
+    struct IsSame
+    {
+        static constexpr bool value = false;
+    };
+
+    template <typename T>
+    struct IsSame<T, T>
+    {
+        static constexpr bool value = true;
+    };
+
+    template <typename T, typename... U>
+    struct AllSame
+    {
+        static constexpr bool value = false;
+    };
+    template <typename T>
+    struct AllSame<T>
+    {
+        static constexpr bool value = true;
+    };
+
+    template <typename T, typename U, typename... Us>
+    struct AllSame<T, U, Us...>
+    {
+        static constexpr bool value = IsSame<T, U>::value && AllSame<T, Us...>::value;
+    };
+
+    // template <typename T, typename U, typename... Us>
+    // struct IsSame<T, U, Us...>
+    //{
+    //     static constexpr bool value = IsSame<T, U> && IsSame<T, Us...>::value;
+    // };
+
+    template <typename X, typename Y>
+    struct Equal
+    {
+        static constexpr bool value = false;
+    };
+
+    template <typename X>
+    struct Equal<X, X>
+    {
+        static constexpr bool value = true;
+    };
+
+    template <typename T>
+    struct HasFunction
+    {
+    private:
+        static char begin(...);
+        template <typename U>
+        static decltype(std::declval<U>().begin()) begin(const U &);
+
+        static char end(...);
+        template <typename U>
+        static decltype(std::declval<U>().end()) end(const U &);
+
+        static char size(...);
+        template <typename U>
+        static decltype(std::declval<U>().size()) size(const U &);
+
+    public:
+        static constexpr bool value =
+            !IsSame<char, decltype(begin(std::declval<T>()))>::value &&
+            !IsSame<char, decltype(end(std::declval<T>()))>::value &&
+            !IsSame<char, decltype(size(std::declval<T>()))>::value;
+    };
+
+    template <typename T>
+    typename EnableIf<HasFunction<T>::value && !IsSame<T, std::string>::value, void>::type
+    print_ip(T val)
+    {
+        std::cout << "Vector/List size " << val.size() << "\n";
+    }
+
+    namespace Number
+    {
+        template <typename NoInteger>
+        inline constexpr bool IsInt = false;
+        // template <>
+        // inline constexpr bool IsInt<int> = true;
+        template <>
+        inline constexpr bool IsInt<int8_t> = true;
+        template <>
+        inline constexpr bool IsInt<int16_t> = true;
+        template <>
+        inline constexpr bool IsInt<int32_t> = true;
+        template <>
+        inline constexpr bool IsInt<int64_t> = true;
+    }
+
+    template <typename T>
+    typename EnableIf<Number::IsInt<T>, void>::type
+    // typename EnableIf<std::is_integral<T>(), void>::type
+    print_ip(T val)
+    {
+        std::cout << "Int =" << val << "\n";
+    }
+
+    template <typename T>
+    typename EnableIf<IsSame<T, std::string>::value, void>::type
+    print_ip(T val)
+    {
+        std::cout << "std::string =" << val << "\n";
+    }
+
+    // template <typename T>
+    // struct has_begin
+    //{
+    // private:
+    //     // Перегрузка с многоточием для случая, когда begin() не существует
+    //     static int check(...);
+    //
+    //    // Специализация для случая, когда begin() существует
+    //    template <typename U>
+    //    static decltype(std::declval<U>().begin()) check(const U &);
+    //
+    // public:
+    //    // Проверяем, что check() возвращает не int (это значит, что begin() существует)
+    //    static constexpr bool value = !std::is_same<int, decltype(check(std::declval<T>()))>::value;
+    //};
+
+    namespace Tuple
+    {
+        template <typename NoIntegerTuple>
+        inline constexpr bool IsTuple = false;
+
+        template <typename U>
+        inline constexpr bool IsTuple<std::tuple<U>> = true;
+
+        template <typename... U>
+        inline constexpr bool IsTuple<std::tuple<U...>> = true;
+    };
+
+    // template <typename T>
+    // typename EnableIf<Tuple::IsTuple<T>, void>::type
+    // print_ip(const T &val)
+    //{
+    //     std::cout << "tuple =" << std::get<0>(val) << " , " << std::get<1>(val) << "\n";
+    // }
+
+    template <typename T, typename... U>
+    typename EnableIf<AllSame<T, U...>::value, void>::type
+    // void
+    print_ip(const std::tuple<T, U...> &val)
+    {
+        std::cout << "tuple =" << std::get<0>(val) << " , " << std::get<1>(val) << "\n";
+    }
+}
--- sfinae.cpp
+++ sfinae.cpp
@@ -1,11 +1,124 @@
 #include <iostream>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <list>
+
+#include <tuple>
+
 #include "lib.h"
+#include "lib_sfinae.h"
 
 using namespace std;
 
 int main()
 {
-    std::cout << "v\n";
+    std::string str = "123";
+
+    // int8_t x_8 = -1;
+    // int16_t x_16 = 0;
+    // int32_t x_32 = 2130706433;
+    // int64_t x_64 = 8875824491850138409;
+
+    int a = 1;
+    otus::print_ip(a);
+
+    int8_t x_8 = -1;
+    otus::print_ip(x_8);
+
+    int16_t x_16 = 0;
+    otus::print_ip(x_16);
+
+    int32_t x_32 = 2130706433;
+    otus::print_ip(x_32);
+
+    int64_t x_64 = 8875824491850138409;
+    otus::print_ip(x_64);
+
+    std::vector v{1, 2, 3};
+    otus::print_ip(v);
+
+    std::vector l{4, 6, 6};
+    otus::print_ip(l);
+
+    std::string s = "Hello world!";
+    otus::print_ip(s);
+
+    // float f = 1.0f;
+    // otus::print_ip(f);
+
+    std::tuple<int, int> t{1, 1};
+    otus::print_ip(t);
+
+    std::cout << "HasFoo has foo(): " << otus::HasFunction<std::vector<int>>::value << std::endl; // true
+    std::cout << "HasFoo has foo(): " << otus::HasFunction<int>::value << std::endl;              // true
+
+    return 0;
+}
+
+int main_int64_t()
+
+{
+    std::string str;
+
+    int64_t x = 8875824491850138409;
+    unsigned char bytes[sizeof(x)];
+    std::copy(&x,
+              &x + sizeof(x),
+              bytes);
+
+    for (int i = 0; i != sizeof(x); ++i)
+    {
+        std::cout << static_cast<int>(bytes[i]) << " ";
+    }
+
+    return 0;
+}
+
+int main_int32_t()
+{
+    int32_t x = 2130706433;
+    unsigned char bytes[sizeof(x)];
+    std::copy(static_cast<const unsigned char *>(static_cast<const void *>(&x)),
+              static_cast<const unsigned char *>(static_cast<const void *>(&x)) + sizeof(x),
+              bytes);
+
+    for (int i = 0; i != sizeof(x); ++i)
+    {
+        std::cout << static_cast<int>(bytes[i]) << " ";
+    }
+
+    return 0;
+}
+
+int main_int_16()
+{
+    int16_t x = 0;
+    unsigned char bytes[sizeof(x)];
+    std::copy(static_cast<const unsigned char *>(static_cast<const void *>(&x)),
+              static_cast<const unsigned char *>(static_cast<const void *>(&x)) + sizeof(x),
+              bytes);
+
+    for (int i = 0; i != sizeof(x); ++i)
+    {
+        std::cout << static_cast<int>(bytes[i]) << " ";
+    }
+
+    return 0;
+}
+
+int main_int_8()
+{
+    int8_t x = -1;
+    unsigned char bytes[sizeof(x)];
+    std::copy(static_cast<const char *>(static_cast<const void *>(&x)),
+              static_cast<const char *>(static_cast<const void *>(&x)) + sizeof x,
+              bytes);
+
+    for (int i = 0; i != sizeof(x); ++i)
+    {
+        std::cout << static_cast<int>(bytes[i]) << " ";
+    }
 
     return 0;
 }
